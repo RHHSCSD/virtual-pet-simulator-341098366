a856e0b67fa1750a2002f662714ee999e4784a41
maxya <maxya@10.0.0.92>
2024-04-12 23:46:30 -0400

    CS

--- src/virtualpet/VirtualPet.java
+++ src/virtualpet/VirtualPet.java
@@ -22,18 +22,26 @@ public class VirtualPet {
         
         char consonant;
         
-        int attempts = 3;
+        int attempts = 3;   
         int randomNumber = 0;
-        int guess = 0;
+        int numberGuess = 0;
+        int money = 0;
+        int matchingIndex = 0;
+        int matchingGuess1 = 0;
+        int matchingGuess2 = 0;
+        int matchingGuesses = 15;
         
         String pet = "";
         String username = "";
         String password = "";
         String name = "";
         String vowel = "";
+        String matching = "aabbccddee";
+        String matchingMixed = "";
+        String matchingDisplay = "";
         
         final int startingPoints = 20;
-        final int guesses = 15;
+        final int numberGuesses = 15;
         final String correctUsername = "snoopy";
         final String correctPassword = "toto";
         
@@ -41,7 +49,17 @@ public class VirtualPet {
         Random rn = new Random();
         
         //Welcome screen
-        System.out.println(" ,_     _\n" +" |\\\\_,-~/\n" +" / _  _ |    ,--.\n" +"(  @  @ )   / ,-'\n" +" \\  _T_/-._( (\n" +" /         `. \\\n" +"|         _  \\ |\n" +" \\ \\ ,  /      |\n" +"  || |-_\\__   /\n" +" ((_/`(____,-'");
+        System.out.println("""
+                            ,_     _
+                            |\\\\_,-~/
+                            / _  _ |    ,--.
+                           (  @  @ )   / ,-'
+                            \\  _T_/-._( (
+                            /         `. \\
+                           |         _  \\ |
+                            \\ \\ ,  /      |
+                             || |-_\\__   /
+                            ((_/`(____,-'""");
         System.out.println("Ultra Silent Pet Simulator Infinity Rebirth V2 Redux");
         //login screen
 //        while(attempts !=0){
@@ -209,43 +227,105 @@ public class VirtualPet {
             int minigameChoice = kb.nextInt();
             switch (minigameChoice){
                 case 1:
+                    //random number guessing game
+                    System.out.println("You've selected the number guessing game");
                     randomNumber = rn.nextInt(100)+1;
-                    for (int i=0; i<=guesses; i++){
-                        if (i == guesses){
+                    for (int i=0; i<=numberGuesses; i++){
+                        //no guesses remaining
+                        if (i == numberGuesses){
                             System.out.println("You have 0 guesses remaining. You Lose!\nThe number was "+randomNumber);
                             System.out.println("Score: 0");
                         }
-                        else if (i < guesses){
-                            System.out.print("Guess a number between 1 and 100. You have " + (guesses - i) + " guesses remaining: ");
-                            guess = kb.nextInt();
-                            if (guess == randomNumber) {
+                        else if (i < numberGuesses){
+                            System.out.print("Guess a number between 1 and 100. You have " + (numberGuesses - i) + " guesses remaining: ");
+                            numberGuess = kb.nextInt();
+                            //if guess is correct
+                            if (numberGuess == randomNumber) {
                                 System.out.println("You guessed correctly! The number was "+randomNumber+". You Win!");
-                                System.out.println("Score: "+ (100*(guesses-i)));
+                                System.out.println("Score: "+ (100*(numberGuesses-i)));
                                 break;
                             }
+                            //if guess is incorrect, tell user whether the number is higher or lower
                             else {
-                                if (guess > randomNumber) {
-                                    System.out.println("The number is lower than " + guess);
+                                if (numberGuess > randomNumber) {
+                                    System.out.println("The number is lower than " + numberGuess);
                                 }
-                                else if (guess < randomNumber) {
-                                    System.out.println("The number is higher than " + guess);
+                                else if (numberGuess < randomNumber) {
+                                    System.out.println("The number is higher than " + numberGuess);
                                 }
-
                             }
                         }                        
                     }
-                case 2:
-                    //
-                default:
                     break;
+                case 2:
+                    //matching game
+                    System.out.println("You've selected the matching game");
+                    //reseting variables
+                    matching = "aabbccddee";
+                    matchingMixed = "";
+                    int matchingLength = matching.length();
+                    matchingGuesses = 15;
+                    matchingDisplay = "";
+                    for (int i=0; i<matchingLength; i++){
+                        matchingDisplay = matchingDisplay + "X";
+                    }
+                    //shuffling the string
+                    while (matchingMixed.length()!=matchingLength){
+                        matchingIndex = rn.nextInt(matching.length());
+                        matchingMixed = matchingMixed + matching.charAt(matchingIndex);
+                        matching = matching.substring(0,matchingIndex) + matching.substring(matchingIndex+1);
+                    }
+                    //game runs as long as all characters aren't revealed
+                    while (!matchingDisplay.equals(matchingMixed)){
+                        //when you run out of guesses
+                        if (matchingGuesses == 0){
+                            System.out.println("You have 0 guesses remaining. You Lose!\nScore: 0");
+                            break;
+                        }
+                        System.out.println(matchingDisplay);
+                        System.out.println("Guess ("+matchingGuesses+" guesses remaining): ");
+                        matchingGuess1 = kb.nextInt();
+                        matchingGuess2 = kb.nextInt();
+                        //if they input a number out of range
+                        if (matchingGuess1<0 || matchingGuess1>=matchingLength || matchingGuess2<0 || matchingGuess2>=matchingLength){
+                            System.out.println("Invalid guesses. Try again");
+                        }
+                        //if they input a number that corresponds to an already correct guess
+                        else if (matchingDisplay.charAt(matchingGuess1)!='X' || matchingDisplay.charAt(matchingGuess2)!='X'){
+                            System.out.println("You guessed an already correct guess. Try again");
+                        }
+                        //if they input 2 of the same number
+                        else if (matchingGuess1 == matchingGuess2){
+                            System.out.println("You guessed the same number. Why you dumb? Try again");
+                        }
+                        else {
+                            //if the guesses match
+                            if (matchingMixed.charAt(matchingGuess1) == matchingMixed.charAt(matchingGuess2)){
+                                //modify display to show correct guesses
+                                matchingDisplay = matchingDisplay.substring(0, matchingGuess1) + matchingMixed.charAt(matchingGuess1) + matchingDisplay.substring(matchingGuess1 + 1);
+                                matchingDisplay = matchingDisplay.substring(0, matchingGuess2) + matchingMixed.charAt(matchingGuess2) + matchingDisplay.substring(matchingGuess2 + 1);
+                                System.out.println("You found a pair!");
+                                matchingGuesses--;
+                                //if all pairs are found
+                                if (matchingDisplay.equals(matchingMixed)){
+                                    System.out.println("You found all pairs! You Win!");
+                                    System.out.println("Score: work in progress");
+                                }
+                            }
+                            //if the guesses don't match
+                            else {
+                                System.out.println("No match. The characters were "+matchingMixed.charAt(matchingGuess1)+" and "+matchingMixed.charAt(matchingGuess2));
+                                matchingGuesses--;
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    System.out.println("THIS IS THE POLICE! GET ON THE GROUND! YOU'RE UNDER ARREST FOR ILLEGAL GAMBLING!");
+                    System.out.println("You got sent to jail for the next 20 years\n*Bad Ending*");
+                    System.exit(0);
             }
-            
         }
-        
-        
-       
-        
-    
         // TODO code application logic here
     }
     
